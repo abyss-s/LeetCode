71ec0b39e2feb0ba72b3ee6a1f2d7385f6110f2d
Youngju Lee <lh3729@naver.com>
2025-09-08 22:51:22 +0900

    1091-shortest-path-in-binary-matrix: 147 ms (79.02%) - 18.4 MB (64.18%) at 09-08-2025

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -6,31 +6,25 @@ class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         n = len(grid)
 
-        # 시작점, 끝점이 막혀있다면 연결 불가
         if grid[0][0] == 1 or grid[-1][-1] == 1:
             return -1
 
+        # 8방향 이동
         dx = [1, 1, 1, -1, -1, -1, 0, 0]
         dy = [0, 1, -1, 0, 1, -1, 1, -1]
 
-        visited = set()
         q = deque([(0, 0, 1)])
+        grid[0][0] = 1  # 방문 처리 
 
         while q:
-            x, y, cur = q.popleft()
-
-            if grid[x][y] == 1:
-                continue
-            if (x, y) in visited:
-                continue
+            x, y, dist = q.popleft()
             if (x, y) == (n - 1, n - 1):
-                return cur
-
-            visited.add((x, y))
+                return dist
 
             for i in range(8):
-                ax, ay = x + dx[i], y + dy[i]
-                if 0 <= ax < n and 0 <= ay < n:
-                    q.append((ax, ay, cur + 1))
+                nx, ny = x + dx[i], y + dy[i]
+                if 0 <= nx < n and 0 <= ny < n and grid[nx][ny] == 0:
+                    grid[nx][ny] = 1  # 방문 처리
+                    q.append((nx, ny, dist + 1))
 
         return -1
